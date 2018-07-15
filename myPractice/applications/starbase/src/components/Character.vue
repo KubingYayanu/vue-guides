6ecc7654c2da7a016a6826a85acc26e12d12b1ac
Kubing Lin <t6m8@hotmail.com>
2018-07-15 12:36:08 +0800

    add extension functionality

--- myPractice/applications/starbase/src/components/Character.vue
+++ myPractice/applications/starbase/src/components/Character.vue
@@ -8,17 +8,18 @@
             <p class="card-text">Hair Color: {{character.hair_color}}</p>
             <p class="card-text">Eye Color: {{character.eye_color}}</p>
           </div>
+          <button class="btn btn-primary" @click.stop="lastCharacter">Back</button>
         </div>
     </div>
 </template>
-
 
 <script>
 export default {
   props: ["id"],
   data() {
     return {
-      character: {}
+      character: {},
+      history_id: []
     };
   },
   methods: {
@@ -31,10 +32,21 @@ export default {
     },
     switchCharacter() {
       let random_id = Math.floor(Math.random() * 83) + 1;
+      this.history_id.push(random_id);
       this.fetchCharacter(random_id);
+    },
+    lastCharacter() {
+      let last_id;
+      if (this.history_id.length > 1) {
+        last_id = this.history_id.pop();
+      } else {
+        last_id = this.history_id[0];
+      }
+      this.fetchCharacter(last_id);
     }
   },
   created() {
+    this.history_id.push(this.id);
     this.fetchCharacter(this.id);
   }
 };
