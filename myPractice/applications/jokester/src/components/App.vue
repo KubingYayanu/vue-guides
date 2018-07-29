af4b456d6ff7d7b322fea08f2c314072bd565053
Kubing Lin <t6m8@hotmail.com>
2018-07-29 22:18:58 +0800

    implement application jokester

--- myPractice/applications/jokester/src/components/App.vue
+++ myPractice/applications/jokester/src/components/App.vue
@@ -1,9 +1,34 @@
 <template>
-    <div id="app">
-        Got jokes?
+  <div id="app">
+    <h3>Got jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
+    <br>
+    <span v-for="(type, index) in types" :key="index">
+      <input type="checkbox" :value="type" v-model="checkedTypes" checked>
+      <label>{{type}}</label>&nbsp;
+    </span>
+    <br>
+    <div class="col-md-12">
+      <Joke v-for="(joke, index) in $store.state.jokes" v-show="checkedTypes.includes(joke.type)" :joke="joke" :index="index" :key="index"></Joke>
     </div>
+  </div>
 </template>
 
 <script>
-    
+import { mapActions } from 'vuex';
+import Joke from './Joke.vue';
+
+export default {
+  data() {
+    return {
+      types: ['general', 'knock-knock', 'programing'],
+      checkedTypes: ['general', 'knock-knock', 'programing']
+    };
+  },
+  methods: mapActions(['initJokes', 'addJoke']),
+  components: {
+    Joke
+  }
+};
 </script>
